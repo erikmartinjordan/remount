9fe3ca740826e94c83fe48ea194f363c0195e897
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2022-03-25 10:33:49 +0100

    Creates privacy page

---
+++ pages/privacy.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import logo from '../public/logo.svg'
+import Link from 'next/link'
+import styles from '../styles/privacy.module.css'
+
+const Privacy = () => {
+
+    return(
+        <div className = {styles.Privacy}>
+            <Link href = '/'><Image src = {logo} alt = 'logo' height = {30} width = {30}/></Link>
+            <h2>Do you upload the images to a server?</h2>
+            <p>No, they remain in your browser.</p>
+            <h2>What does this track?</h2>
+            <p>Number of pageviews and number of total downloads.</p>
+            <h2>Why?</h2>
+            <p>To know if people are using the app.</p>
+            <h2>Where is the data stored?</h2>
+            <p>Firebase.</p>
+            <h2>Why privacy pages are so boring?</h2>
+            <p>😅</p>
+        </div>
+    )
+
+}
+
+export default Privacy
